6797f2b72ad122160b03e0b751008679e79d15f8
raul4h <rjhinostrozagarcia@miners.utep.edu>
2017-02-20 21:07:53 -0700

    Read "Play" URL and store input

--- readURL.php
+++ readURL.php
@@ -3,8 +3,9 @@
 $url = " play/?pid=55ed3eb95f5a3&shot=4,5";
 $input = explode("=", $url);  //input[1] = 55ed3eb95f5a3&shot , input[2] = 4,5
 $divider = explode("&", $input[1]); // divider[0] = 55ed3eb95f5a3
-$pid = $divider[0];
 $coordinates = explode(",", $input[2]);
+
+$pid = $divider[0];
 $x = $coordinates[0];
 $y = $coordinates[1];
 
