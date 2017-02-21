2cafb61a96bf07ccdba5632489760d9601d7e5e5
Marina Chong <mschong5@hotmail.com>
2017-02-20 20:33:54 -0700

    Missing "Ship deployment not well-formed" error.

--- new/index.php
+++ new/index.php
@@ -1,11 +1,28 @@
 <?php
-// $url = " new?strategy=Smart&ships=Aircraft+carrier,1,6,false;Battleship,7,5,true;Frigate,2,1,false;Submarine,9,6,false;Minesweeper,10,9,false";
-$url = " new?strategy=Smart&";
+$url = " new?strategy=Smart&ships=Aircraft+carrier,1,6,true;Battleship,7,5,true;Frigate,2,1,false;Submarine,9,6,false;Minesweeper,10,9,false";
+// $url = " new?strategy=Smart&";
 
 $input = explode ( "?", $url );
 
 $strategy = explode ( "&", $input [1] );
 
+$board = createBoard ( 10 );
+
+//Check if ship info is provided
+if ($strategy [1] == null) {
+	$s = createRandShips ();
+	
+	$board = randomPlacement ( 10, $s );
+	
+	for($i = 1; $i <= 10; $i ++) {
+		for($j = 1; $j <= 10; $j ++)
+			echo $board [$i] [$j];
+		echo "\n";
+	}
+	exit ();
+}
+
+// Strategy not specified
 if ($strategy [0] == null) {
 	$strategyNotSpecified = array (
 			"response" => false,
@@ -14,13 +31,9 @@ if ($strategy [0] == null) {
 	exit ( json_encode ( $strategyNotSpecified ) );
 }
 
-if ($strategy [1] == null) {
-	echo "Hola\n";
-	prettyPrint ( createShipsNC ( 10 ) );
-}
-
 $typeStrategy = explode ( "=", $strategy [0] );
 
+// Unknown strategy
 if ($typeStrategy [1] != "Smart" && $typeStrategy [1] != "Sweep" && $typeStrategy [1] != "Random") {
 	$unknownStrategy = array (
 			"response" => false,
@@ -29,14 +42,20 @@ if ($typeStrategy [1] != "Smart" && $typeStrategy [1] != "Sweep" && $typeStrateg
 	exit ( json_encode ( $unknownStrategy ) );
 }
 
-// echo $typeStrategy[1];
-
 $ships = explode ( "=", $strategy [1] );
 
 $ship = explode ( ";", $ships [1] );
 
 list ( $name, $x, $y, $direction ) = explode ( ",", $ship [0] );
+$shipSize = array (
+		"Aircraft+carrier" => 5,
+		"Battleship" => 4,
+		"Frigate" => 3,
+		"Submarine" => 3,
+		"Minesweeper" => 2 
+);
 
+// Unknown ship name
 if ($name != "Aircraft+carrier" && $name != "Battleship" && $name != "Frigate" && $name != "Submarine" && $name != "Minesweeper") {
 	$uknownName = array (
 			"response" => false,
@@ -45,19 +64,26 @@ if ($name != "Aircraft+carrier" && $name != "Battleship" && $name != "Frigate" &
 	exit ( json_encode ( $uknownName ) );
 }
 
-if($direction == true){
-	
-	
-	
-}
-if ($x > 10 || $x < 1 || $y > 10 || $y < 1) {
-	$invalidPos = array (
-			"response" => false,
-			"reason" => "Invalid ship position" 
-	);
-	exit ( json_encode ( $invalidPos ) );
+// Invalid ship position
+if ($direction == true) {
+	if ($x + $shipSize [$name] > 10 || $x < 1 || $y > 10 || $y < 1) {
+		$invalidPos = array (
+				"response" => false,
+				"reason" => "Invalid ship position" 
+		);
+		exit ( json_encode ( $invalidPos ) );
+	}
+} else {
+	if ($x > 10 || $x < 1 || $y + $shipSize [$name] > 10 || $y < 1) {
+		$invalidPos = array (
+				"response" => false,
+				"reason" => "Invalid ship position" 
+		);
+		exit ( json_encode ( $invalidPos ) );
+	}
 }
 
+// Invalid ship direction
 if ($direction != "true" && $direction != "false") {
 	$invalidDir = array (
 			"response" => false,
@@ -66,6 +92,25 @@ if ($direction != "true" && $direction != "false") {
 	exit ( json_encode ( $invalidDir ) );
 }
 
+// Place ships in board
+// Error: Conflicting ship deployments
+$shipsArray = array ();
+$i = 0;
+foreach ( $ship as $individualShip ) {
+	list ( $name, $x, $y, $direction ) = explode ( ",", $individualShip );
+	$direction = $direction === 'true';
+	$shipsArray [$i] = new Ship ( $name, $x, $y, $direction, $shipSize [$name] );
+	if (! placeInBoard ( $board, $shipsArray [$i] )) {
+		$conflicting = array (
+				"response" => false,
+				"reason" => "Conflicting ship deployments" 
+		);
+		exit ( json_encode ( $conflicting ) );
+	}
+	$i ++;
+}
+
+// Incomplete ship deployments
 if (count ( $ship ) != 5) {
 	$incompleteShips = array (
 			"response" => false,
@@ -74,28 +119,45 @@ if (count ( $ship ) != 5) {
 	exit ( json_encode ( $incompleteShips ) );
 }
 
-$shipsArray = array ();
-$i = 0;
-foreach ( $ship as $individualShip ) {
-	list ( $name, $x, $y, $direction ) = explode ( ",", $individualShip );
-	$shipsArray [$i] = new shipInfo ();
-	$shipsArray [$i]->name = $name;
-	$shipsArray [$i]->x = $x;
-	$shipsArray [$i]->y = $y;
-	$shipsArray [$i]->dir = $direction;
-	$i ++;
-}
-// echo $shipsArray [0]->name;
-class shipInfo {
-	public $name;
-	public $x;
-	public $y;
-	public $dir;
+for($i = 0; $i < count ( $ship ); $i ++) {
+	for($j = $i + 1; $j < count ( $ship ); $j ++) {
+		if ($shipsArray [$i]->name == $shipsArray [$j]->name){
+			$incompleteShips = array (
+					"response" => false,
+					"reason" => "Incomplete ship deployments"
+			);
+			exit ( json_encode ( $incompleteShips ) );
+		}
+	}
 }
 
-
-
-
+$success = array (
+		"response " => true,
+		"pid" => uniqid ( round ( microtime ( true ) * 1000 ) ) 
+);
+exit ( json_encode ( $success ) );
+function prettyPrint($board) {
+	for($i = 1; $i <= 10; $i ++) {
+		for($j = 1; $j <= 10; $j ++)
+			echo $board [$i] [$j];
+		echo "\n";
+	}
+}
+function placeInBoard(&$board, $ship) {
+	if (! isAvailable ( $board, $ship->size, $ship->x, $ship->y, $ship->horizontal )) {
+		return false;
+	}
+	
+	if (! $ship->horizontal) {
+		for($i = 0; $i < $ship->size; $i ++)
+			$board [$ship->y + $i] [$ship->x] = $ship->name;
+	} else {
+		for($i = 0; $i < $ship->size; $i ++)
+			$board [$ship->y] [$ship->x + $i] = $ship->name;
+	}
+	
+	return true;
+}
 class Ship { // Ship object. One for every ship, 5 per board per game.
 	public $horizontal; // if false, ship is vertical
 	public $name; // Aircraft carrier, etc.
@@ -112,123 +174,100 @@ class Ship { // Ship object. One for every ship, 5 per board per game.
 		$this->size = $size;
 	}
 }
-function randomPlacement($boardSize, &$ships){
-
-
-	$board = createBoard($boardSize);
-
-	foreach ($ships as $individualShip)
-		placeRandomly($board, $individualShip);
-
-		return $board;
-
+function randomPlacement($boardSize, &$ships) {
+	$board = createBoard ( $boardSize );
+	
+	foreach ( $ships as $individualShip )
+		placeRandomly ( $board, $individualShip );
+	
+	return $board;
 }
 function createRandShips() {
-
-	echo"Enter";
-
 	$shipNames = array (
-				
+			
 			"Aircraft+carrier",
 			"Battleship",
 			"Frigate",
 			"Submarine",
-			"Minesweeper"
+			"Minesweeper" 
 	);
-
+	
 	$shipSize = array (
 			5,
 			4,
 			3,
 			3,
-			2
+			2 
 	);
-	$ships = array (
-	);
-
-	for($i = 0; $i <= 4; $i++) {
-		$ships[$i] = new Ship ( $shipNames [$i], 0, 0, true, $shipSize [$i] );
-
-
+	$ships = array ();
+	
+	for($i = 0; $i <= 4; $i ++) {
+		$ships [$i] = new Ship ( $shipNames [$i], 0, 0, true, $shipSize [$i] );
 	}
-
+	
 	return $ships;
-
-
 }
 function createBoard($boardSize) {
 	$board = array (
-			array ()
+			array () 
 	);
-
+	
 	for($i = 0; $i <= $boardSize; $i ++)
 		for($j = 0; $j <= $boardSize; $j ++) {
-				
+			
 			$board [$i] [$j] = 0;
 		}
-
+	
 	return $board;
 }
 function placeRandomly(&$board, &$ship) {
 	$randomXcoordinate;
 	$randomYcoordinate;
-
-
-
+	
 	$randomDirection = rand ( 0, 1 );
 	$ship->horizontal = $randomDirection;
-
+	
 	if ($ship->vertical == true) {
 		$randomXcoordinate = rand ( 1, 10 );
 		$randomYcoordinate = rand ( 1, (10 - $ship->size) );
-	}
-	else {
+	} else {
 		$randomXcoordinate = rand ( 1, (10 - $ship->size) );
 		$randomYcoordinate = rand ( 1, 10 );
 	}
-
-	while ( ! isEmpty ( $board, $ship->size, $randomXcoordinate, $randomYcoordinate, $ship->horizontal ) ) {
-
+	
+	while ( ! isAvailable ( $board, $ship->size, $randomXcoordinate, $randomYcoordinate, $ship->horizontal ) ) {
+		
 		if ($ship->horizontal == false) {
 			$randomXcoordinate = rand ( 1, 10 );
-			$randomYcoordinate = rand ( 1, (10 - $$ship ->size) );
-		}
-		else {
+			$randomYcoordinate = rand ( 1, (10 - $ship->size) );
+		} else {
 			$randomXcoordinate = rand ( 1, (10 - $ship->size) );
 			$randomYcoordinate = rand ( 1, 10 );
 		}
 	}
-
-	echo $ship->x = $randomXcoordinate;
-	echo $ship->y = $randomYcoordinate;
-	echo $ship->name;
-
-
-	if (! $ship->horizontal)
-		for($i = 0; $i < $ship ->size; $i ++)
-			$board [$ship->x] [$ship->y + $i] = $ship -> name;
-			else
-				for($i = 0; $i < $ship->size; $i ++)
-					$board [$ship->x + $i] [$ship->y] = $ship ->name;
+	$ship->x = $randomXcoordinate;
+	$ship->y = $randomYcoordinate;
+	
+	placeInBoard ( $board, $ship );
 }
-function isEmpty($board, $size, $rx, $ry, $horizontal) {
-
+function isAvailable($board, $size, $rx, $ry, $horizontal) {
+	
 	// This method will return false if the cell is already occupied.
 	if (! $horizontal) {
-
+		
 		for($i = 0; $i < $size; $i ++) {
 			if ($board [$rx] [$ry + $i])
 				return false;
 		}
-
+		
 		return true;
 	} else {
-
+		
 		for($i = 0; $i < $size; $i ++) {
 			if ($board [$rx + $i] [$ry])
 				return false;
 		}
-
+		
 		return true;
 	}
 }
