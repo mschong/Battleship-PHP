fec6c44ef232023380a2c9a93a289d4417859e86
raul4h <rjhinostrozagarcia@miners.utep.edu>
2017-02-21 22:35:28 -0700

    Some methods I think could help

---
+++ play/board.php
@@ -0,0 +1,39 @@
+<?php
+
+$board;
+
+function fillBoard($x,$y,$size,$direction,$name){
+	
+	$letter = substr($name, 0);
+	
+	for ($i = 0; $i < $size; $i++) {
+		if ($direction == true){
+			$board[$x][$y+$i] = $letter;
+		}
+		else{
+			$board[$x+$i][$y] = $letter;
+		}
+	}
+}
+
+function visited($x,$y){
+	
+	if ($board[$x][$y]){
+		return true;
+	}
+	else{
+		$board[$x][$y] = 1;
+		return false;
+	}
+}
+
+function isThereShip($x,$y){
+	if (is_string($board[$x][$y])){
+		return true;
+	}
+	else{
+		return false;
+	}
+}
+
+?>
