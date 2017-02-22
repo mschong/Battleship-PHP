640a2915b5907e5a2b3fdef61ea6d09367990210
Marina Chong <mschong5@hotmail.com>
2017-02-21 17:10:44 -0700

    Read file with ships info and pid

--- play/index.php
+++ play/index.php
@@ -1,33 +1,52 @@
 <?php
-
 $pid = $_GET ['pid'];
 $shot = $_GET ['shot'];
-$coord = explode(",", $shot);
+$coord = explode ( ",", $shot );
+$gameInfo = fopen("../new/$pid", "r");
 
-
-//Pid not specified
-if($pid == null){
-	$noPid = array("response" => false, "reason" => "Pid not specified");
-	exit( json_encode($noPid));
+// Pid not specified
+if ($pid == null) {
+	$noPid = array (
+			"response" => false,
+			"reason" => "Pid not specified" 
+	);
+	exit ( json_encode ( $noPid ) );
 }
 
-//Shot not specified
-if ($shot == null){
-	$noShot = array("response" => false, "reason" => "Shot not specified");
-	exit( json_encode($noShot));
+// Unknown pid
+if (! $gameInfo ) {
+	$unknownPid = array (
+			"responer" => false,
+			"reason" => "Unknown pid" 
+	);
+	exit ( json_encode ( $unknownPid ) );
 }
 
-//Shot not well-formed
-if(count($coord) != 2){
-	$shotNotWellFormed = array("response" => false, "reason" => "Shot not well-formed");
-	exit(json_encode($shotNotWellFormed));
+// Shot not specified
+if ($shot == null) {
+	$noShot = array (
+			"response" => false,
+			"reason" => "Shot not specified" 
+	);
+	exit ( json_encode ( $noShot ) );
 }
 
-//Invalid shot position
-if($coord[0] < 1 || $coord[0] > 10 || $coord[1] < 1 || $coord[1] > 10 ){
-	$invalidShot = array("response" => false, "reason" => "Invalid shot position");
-	exit(json_encode($invalidShot));
+// Shot not well-formed
+if (count ( $coord ) != 2) {
+	$shotNotWellFormed = array (
+			"response" => false,
+			"reason" => "Shot not well-formed" 
+	);
+	exit ( json_encode ( $shotNotWellFormed ) );
 }
 
+// Invalid shot position
+if ($coord [0] < 1 || $coord [0] > 10 || $coord [1] < 1 || $coord [1] > 10) {
+	$invalidShot = array (
+			"response" => false,
+			"reason" => "Invalid shot position" 
+	);
+	exit ( json_encode ( $invalidShot ) );
+}
 
 ?>
