bae9729e99f8a16e1361968f6ba612e25b6350b2
Marina Chong <mschong5@hotmail.com>
2017-02-22 23:00:27 -0700

    Almost

--- play/board.php
+++ play/board.php
@@ -1,32 +1,27 @@
 <?php
 
+
 function createBoard($size){
 	$board = array(array());
 	for ($i = 1; $i <= $size; $i++) {
 		for ($j = 1; $j <= $size; $j++) {
-			$board[$i][$j] = 0;
+			$board[$i][$j] = new Place(0, null);
 		}
 	}
 	return $board;
 }
 
-function fillBoard($boardToFill, $x,$y,$size,$direction,$name){
+function fillBoard($boardToFill, $ship){
 	
-	$letter = $name[0];
-	$intx = intval($x);
-	$inty = intval($y);
-	for ($i = 0; $i < $size; $i++) {
-		if ($direction == true){
-			$boardToFill[$intx+$i][$inty] = $letter;
-// 			echo $boardToFill[$intx+$i][$inty];
-// 			echo"\n";
-// 			printBoard($boardToFill);
+	$intx = intval($ship->x);
+	$inty = intval($ship->y);
+	for ($i = 0; $i < $ship->size; $i++) {
+		if ($ship->horizontal == true){
+			$boardToFill[$intx+$i][$inty]->ship = $ship;
+
 		}
 		else{
-			$boardToFill[$intx][$inty+$i] = $letter;
-// 			echo $boardToFill[$intx+$i][$inty];
-// 			echo"\n";
-// 			printBoard($boardToFill);
+			$boardToFill[$intx][$inty+$i]->ship = $ship;
 		}
 	}
 	return $boardToFill;
@@ -37,7 +32,7 @@ function printBoard($boardToPrint){
 
 	for ($i = 1;$i<=count($boardToPrint);$i++){
 		for ($j = 1;$j<=count($boardToPrint);$j++){
-			echo $boardToPrint[$i][$j];
+			print_r($boardToPrint[$i][$j]) ;
 			echo " ";
 		}
 		echo "\n";
@@ -63,6 +58,57 @@ function isThereShip($x,$y){
 	}
 }
 
+function hitBoard($board, $x, $y){
+	$place = $board[$x][$y];
 
+	if($place->isHit){
+		return 10;
+	}
+	
+	$place->isHit = true;
+	if($place->ship != null){
+		$place->ship->countHits++;
+		if($place->ship->countHits == $place->ship->size){
+			$place->ship->sunk = true;	
+		}
+
+	}
+	
+	return $place->ship;
+}
+
+function createShip($x,$y,$size,$direction,$name){
+	$ship = new Ship($name, $x, $y, $direction, $size);
+	return $ship;
+}
+
+class Ship { // Ship object. One for every ship, 5 per board per game.
+	public $horizontal; // if false, ship is vertical
+	public $name; // Aircraft carrier, etc.
+	public $x; // x coordinate
+	public $y; // y coordinate
+	public $sunk; // if false, ship is alive :D
+	public $size;
+	public $countHits;
+	public function __construct($name, $x, $y, $horizontal, $size) {
+		$this->name = $name;
+		$this->x = $x;
+		$this->y = $y;
+		$this->horizontal = $horizontal;
+		$sunk = 0;
+		$this->size = $size;
+		$this->countHits = 0;
+	}
+}
+
+class Place{
+	public $isHit;
+	public $ship;
+	
+	public function __construct($isHit, $ship){
+		$this->isHit = $isHit;
+		$this->ship = $ship;
+	}
+}
 
 ?>
--- play/index.php
+++ play/index.php
@@ -3,11 +3,14 @@ include 'board.php';
 // $pid = $_GET ['pid'];
 // $shot = $_GET ['shot'];
 $pid = "148772130011158acd3541b28f";
-$shot = "4,5";
+$shot = "6,8";
 $coord = explode ( ",", $shot );
-$gameInfo = fopen("../new/$pid", "r");
-
-$board = createBoard(10);
+$coord[0] = intval($coord[0]);
+$coord[1] = intval($coord[1]);
+$gameInfo = fopen ( "../new/$pid", "r" );
+$board = createBoard ( 10 );
+$ships = array ();
+$countSunk = 0;
 
 // Pid not specified
 if ($pid == null) {
@@ -19,25 +22,13 @@ if ($pid == null) {
 }
 
 // Unknown pid
-if (! $gameInfo ) {
+if (! $gameInfo) {
 	$unknownPid = array (
 			"response" => false,
 			"reason" => "Unknown pid" 
 	);
 	exit ( json_encode ( $unknownPid ) );
 }
-
-while (($line = fgets($gameInfo)) != false){
-      $explodedLine = explode(",", $line);
-      if ($explodedLine[3] == '1')
-      	$explodedLine[3] = true;
-      else
-      	$explodedLine[3] = false;
-      
-      $board = fillBoard($board, $explodedLine[1], $explodedLine[2], $explodedLine[4],$explodedLine[3],$explodedLine[0]);
-}
-
-printBoard($board);
 
 // Shot not specified
 if ($shot == null) {
@@ -65,5 +56,100 @@ if ($coord [0] < 1 || $coord [0] > 10 || $coord [1] < 1 || $coord [1] > 10) {
 	);
 	exit ( json_encode ( $invalidShot ) );
 }
+
+$i = 0;
+while ( ($line = fgets ( $gameInfo )) != false ) {
+	$explodedLine = explode ( ",", $line );
+	if ($explodedLine [3] == '1')
+		$explodedLine [3] = true;
+	else
+		$explodedLine [3] = false;
+	$ships [$i] = createShip ( $explodedLine [1], $explodedLine [2], $explodedLine [4], $explodedLine [3], $explodedLine [0] );
+	$board = fillBoard ( $board, $ships [$i] );
+	$i ++;
+}
+// printBoard ( $board );
+$hit;
+$hitResponse = hitBoard ( $board, $coord [0], $coord [1] );
+
+if ($hitResponse==10) {
+	$invalidShot = array (
+			"response" => false,
+			"reason" => "Invalid shot position" 
+	);
+	exit ( json_encode ( $invalidShot ) );
+} else {
+	$ship = $hitResponse;
+	if ($ship == null) {
+		$hit = array (
+				"response" => true,
+				"ack_shot" => array (
+						"x" => $coord [0],
+						"y" => $coord [1],
+						"isHit" => false,
+						"isSunk" => false,
+						"isWin" => false,
+						"ship" => array () 
+				) 
+		);
+	}
+	else if($ship->sunk) {
+		$coordArray = array ();
+		$countSunk ++;
+		
+		if ($ship->horizontal) {
+			for($i = 0; $i < 2 * $ship->size; $i ++) {
+				$coordArray [$i] = $ship->x + $i;
+				$coordArray [++ $i] = $ship->y;
+			}
+		} else {
+			for($i = 0; $i < 2 * $ship->size; $i ++) {
+				$coordArray [$i] = $ship->x;
+				$coordArray [++ $i] = $ship->y + $i;
+			}
+		}
+		if ($countSunk == 5) {
+			$hit = array (
+					"response" => true,
+					"ack_shot" => array (
+							"x" => $coord [0],
+							"y" => $coord [1],
+							"isHit" => true,
+							"isSunk" => true,
+							"isWin" => true,
+							"ship" => $coordArray 
+					) 
+			);
+		} else {
+			$hit = array (
+					"response" => true,
+					"ack_shot" => array (
+							"x" => $coord [0],
+							"y" => $coord [1],
+							"isHit" => true,
+							"isSunk" => true,
+							"isWin" => false,
+							"ship" => $coordArray 
+					) 
+			);
+		}
+	} 
+
+	else {
+		$hit = array (
+				"response" => true,
+				"ack_shot" => array (
+						"x" => $coord [0],
+						"y" => $coord [1],
+						"isHit" => true,
+						"isSunk" => false,
+						"isWin" => false,
+						"ship" => array () 
+				) 
+		);
+	}
+}
+
+exit ( json_encode ( $hit ) );
 
 ?>
